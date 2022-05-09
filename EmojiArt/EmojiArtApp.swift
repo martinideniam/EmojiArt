c89157d0789e2474e5606394d6d6570cc9b9c10d
martinideniam <martinideniam@gmail.com>
2022-05-09 19:43:39 +0300

    Initial Commit

---
+++ EmojiArt/EmojiArtApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmojiArtApp.swift
+//  EmojiArt
+//
+//  Created by Vladislav Gorovenko on 09.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct EmojiArtApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
